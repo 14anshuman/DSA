a54e7fab5f41962d3a05097ccad37c9151c6c431
Anshuman Singh <singhanshuman8182@gmail.com>
2025-09-14 17:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (18.70%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a=3,b=2;
+       if(n<=3){
+        return n;
+       } else{
+        for(int i=0 ;i<n-3;i++){
+            a=a+b;
+            b=a-b;
+        }
+       }
+       return a;
+    }
+}
