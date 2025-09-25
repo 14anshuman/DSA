1223910dc6b22ccebe6ce5733bae3268ae385799
Anshuman Singh <singhanshuman8182@gmail.com>
2025-09-25 20:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (29.88%) - LeetSync

---
+++ 343-integer-break/integer-break.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int integerBreak(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+        int[] memo = new int[n + 1];
+        return maxProduct(n, memo);
+    }
+    
+    private int maxProduct(int n, int[] memo) {
+        if (n == 2) {
+            return 1;
+        }
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+        int max = 0;
+        for (int i = 1; i < n; i++) {
+            max = Math.max(max, Math.max(i * (n - i), i * maxProduct(n - i, memo)));
+        }
+        memo[n] = max;
+        return max;
+    }
+}
