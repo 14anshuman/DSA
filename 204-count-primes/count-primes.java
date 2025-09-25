369e43e0e5b13dccc6302d474315f6b8948fb343
Anshuman Singh <singhanshuman8182@gmail.com>
2025-09-25 18:02:19 +0530

    Time: 98 ms (53.04%) | Memory: 49.6 MB (24.24%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;  // no primes below 2
+        
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        
+        // Sieve of Eratosthenes
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+        
+        return count;
+    }
+}
