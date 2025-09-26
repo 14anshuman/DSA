f8a8063df7afe5e650b35b3bf58ff2262bac2ad6
Anshuman Singh <singhanshuman8182@gmail.com>
2025-09-26 13:24:42 +0530

    Time: 9 ms (11.22%) | Memory: 49.4 MB (5.18%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean stoneGame(int[] nums) {
+        int n = nums.length;
+        Integer[][] memo = new Integer[n][n];
+        return scoreDiff(nums, 0, n - 1, memo) >= 0;
+    }
+     private int scoreDiff(int[] nums, int l, int r, Integer[][] memo) {
+        if (l == r) return nums[l];
+        if (memo[l][r] != null) return memo[l][r];
+
+        int pickLeft  = nums[l] - scoreDiff(nums, l + 1, r, memo);
+        int pickRight = nums[r] - scoreDiff(nums, l, r - 1, memo);
+
+        return memo[l][r] = Math.max(pickLeft, pickRight);
+    }
+}
