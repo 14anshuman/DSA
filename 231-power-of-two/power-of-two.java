e080ea1d6f12488e1d5448d80f68a9b8911e1795
Anshuman Singh <singhanshuman8182@gmail.com>
2026-02-03 18:53:19 +0530

    Time: 1 ms (95.61%) | Memory: 42.9 MB (13.91%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,10 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-         for(int i=0;i<=Math.sqrt(n+1);i++){
-            if(Math.pow(2,i)==n){
-                return true;
-            }
-        }
-        return false;
+        return n>0 && (n & (n - 1)) == 0;
     }
 }
